3c6092d9a1821375d28b90ff004266611d79f531
Christian Decker <decker.christian@gmail.com>
2021-03-05 12:00:56 +0100

    cli: Added HTLC settlement detection via nLocktime

--- lntool/cli.py
+++ lntool/cli.py
@@ -1,27 +1,82 @@
 import json
+import logging
+from collections import namedtuple
+
 import click
 import requests
 from pyln.proto.primitives import ShortChannelId
-import logging
 
-logging.basicConfig(format='%(asctime)-15s %(message)s')
+logging.basicConfig(format="%(asctime)-15s %(message)s")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+Outpoint = namedtuple("Outpoint", ["txid", "outnum"])
+
+
+class BlockchainBackend(object):
+    def tx(self, txid):
+        pass
+
+    def scid2txid(self, scid):
+        pass
+
+
+class BlockstreamBackend(BlockchainBackend):
+    def __init__(self):
+        self.session = requests.Session()
+
+    def tx(self, txid):
+        logger.debug(f"Retrieving metadata for tx {txid}")
+        meta = self.session.get(f"https://blockstream.info/api/tx/{txid}").json()
+
+        logger.debug(f"Retrieving spending information for {len(meta['vout'])} outputs")
+        spends = self.session.get(
+            f"https://blockstream.info/api/tx/{txid}/outspends"
+        ).json()
+
+        for out, spend in zip(meta["vout"], spends):
+            out["spent_by"] = spend
+
+        return meta
+
+    def scid2txid(self, scid):
+        logger.debug(f"Resolving blockhash for height {scid.block}")
+        block_hash = self.session.get(
+            f"https://blockstream.info/api/block-height/{scid.block}"
+        ).content.decode("ASCII")
+        logger.debug(f"Blockheight {scid.block} resolved to {block_hash}")
+        block_txs = self.session.get(
+            f"https://blockstream.info/api/block/{block_hash}/txids"
+        ).json()
+        txid = block_txs[scid.txnum]
+        logger.debug(f"Resolved {scid} to {txid}:{scid.outnum}")
+        return (txid, scid.outnum)
+
+    def spends(self, outpoint: Outpoint):
+        """Locate the transaction that spends the outpoint, if any"""
+        spends = self.session.get(
+            f"https://blockstream.info/api/tx/{outpoint.txid}/outspends"
+        ).json()
+        return spends[outpoint.outnum]
+
+    def close_p2wsh(self, outpoint: Outpoint):
+        """Try to identify the type of a P2WSH attached to a unilateral close TX"""
+        spender = self.spends(outpoint)
+        import pdb
+
+        pdb.set_trace()
+
+
 def lookup_tx(txid):
     logger.debug(f"Retrieving metadata for tx {txid}")
-    meta = requests.get(
-        f'https://blockstream.info/api/tx/{txid}'
-    ).json()
+    meta = requests.get(f"https://blockstream.info/api/tx/{txid}").json()
 
     logger.debug(f"Retrieving spending information for {len(meta['vout'])} outputs")
-    spends = requests.get(
-        f'https://blockstream.info/api/tx/{txid}/outspends'
-    ).json()
+    spends = requests.get(f"https://blockstream.info/api/tx/{txid}/outspends").json()
 
-    for out, spend in zip(meta['vout'], spends):
-        out['spent_by'] = spend
+    for out, spend in zip(meta["vout"], spends):
+        out["spent_by"] = spend
 
     return meta
 
@@ -29,11 +84,11 @@ def lookup_tx(txid):
 def resolve_scid(scid):
     logger.debug(f"Resolving blockhash for height {scid.block}")
     block_hash = requests.get(
-        f'https://blockstream.info/api/block-height/{scid.block}'
-    ).content.decode('ASCII')
+        f"https://blockstream.info/api/block-height/{scid.block}"
+    ).content.decode("ASCII")
     logger.debug(f"Blockheight {scid.block} resolved to {block_hash}")
     block_txs = requests.get(
-        f'https://blockstream.info/api/block/{block_hash}/txids'
+        f"https://blockstream.info/api/block/{block_hash}/txids"
     ).json()
     txid = block_txs[scid.txnum]
     logger.debug(f"Resolved {scid} to {txid}:{scid.outnum}")
@@ -42,15 +97,13 @@ def resolve_scid(scid):
 
 @click.group()
 def cli():
-    """A tool to inspect Lightning Network metadata.
-    """
+    """A tool to inspect Lightning Network metadata."""
     pass
 
 
 @cli.group()
 def channels():
-    """Commands related to channel operations and data lookup.
-    """
+    """Commands related to channel operations and data lookup."""
     pass
 
 
@@ -72,29 +125,30 @@ def analyze(short_channel_id):
     added in future.
 
     """
+    backend = BlockstreamBackend()
     scid = ShortChannelId.from_str(short_channel_id)
-    txid, outnum = resolve_scid(scid)
-    funding_tx = lookup_tx(txid)
+    txid, outnum = backend.scid2txid(scid)
+    funding_tx = backend.tx(txid)
 
-    funding_output = funding_tx['vout'][scid.outnum]
-    spender = funding_output['spent_by']
+    funding_output = funding_tx["vout"][scid.outnum]
+    spender = funding_output["spent_by"]
     penalty = False
     lifetime = None
 
-    if spender['spent']:
+    if spender["spent"]:
         logger.debug(f"Funding output was spent, looking up closing tx")
-        close_txid = spender['txid']
-        close_tx = lookup_tx(close_txid)
+        close_txid = spender["txid"]
+        close_tx = backend.tx(close_txid)
 
         # Now look at the close tx, and determine what type of close it is.
-        out_types = [o['scriptpubkey_type'] for o in close_tx['vout']]
+        out_types = [o["scriptpubkey_type"] for o in close_tx["vout"]]
 
-        if 'v0_p2wsh' in out_types:
-            close_type = 'unilateral'
+        if "v0_p2wsh" in out_types:
+            close_type = "unilateral"
         else:
-            close_type = 'collaborative'
+            close_type = "collaborative"
 
-        lifetime = spender['status']['block_height'] - scid.block
+        lifetime = spender["status"]["block_height"] - scid.block
 
         logger.debug(f"Close determined to be {close_type}")
 
@@ -102,56 +156,78 @@ def analyze(short_channel_id):
         # see if it is going to be penalized (branch 1 in the if-else
         # script) or whether the timeout expires and the closing party
         # gets its funds back.
-        for i, out in enumerate(close_tx['vout']):
-            if out['scriptpubkey_type'] != 'v0_p2wsh':
+        for i, out in enumerate(close_tx["vout"]):
+            if out["scriptpubkey_type"] != "v0_p2wsh":
                 # This is the direct return to closee output, no need
                 # to follow that
-                out['type'] = 'direct_to_closee'
+                out["type"] = "direct_to_closee"
                 continue
 
-            if not out['spent_by']['spent']:
+            if not out["spent_by"]["spent"]:
                 # No point in following this link, the output isn't
                 # spent yet, so we can't determine its type.
-                out['type'] = 'unknown'
+                out["type"] = "unknown"
                 continue
 
-            return_tx = lookup_tx(out['spent_by']['txid'])
+            return_tx = backend.tx(out["spent_by"]["txid"])
 
             # Determine which input is the spending one:
             return_index = None
             return_in = None
-            for i, inp in enumerate(return_tx['vin']):
-                if inp['txid'] == close_tx['txid']:
+            for i, inp in enumerate(return_tx["vin"]):
+                if inp["txid"] == close_tx["txid"]:
                     return_index = i
                     return_in = inp
                     break
-            assert(return_index is not None)
-            assert(return_in is not None)
+            assert return_index is not None
+            assert return_in is not None
 
             # The type is defined by the argument passed to OP_IF in
             # the second position of the to_local output. This is
             # either 0x00 (or empty string) for a sweep, and 0x01 for
             # a penalty.
-            return_in = return_tx['vin'][0]
-            if return_in['witness'][1] == '01':
-                return_type = 'penalty'
+            return_in = return_tx["vin"][0]
+            if return_in["witness"][1] == "01":
+                return_type = "penalty"
                 penalty = True
-            else:
-                return_type = 'sweep'
+            elif "OP_HASH160" in return_in["inner_witnessscript_asm"]:
 
-            out['type'] = return_type
+                # Need to look into the spender of this output to see
+                # if it is a timeout or a success
+                # import pdb;pdb.set_trace()
+                # o = Outpoint(return_tx['txid'], 0)
+                # spender = backend.spends(o)
+                # t = backend.tx(spender['txid'])
+
+                return_type = "htlc"
+                if return_tx["locktime"] == 0:
+                    return_type += " (fulfill)"
+                else:
+                    return_type += " (timeout)"
+            else:
+                return_type = "sweep"
+            logging.debug(f"Close output {i} identified as {return_type}")
+
+            out["type"] = return_type
     else:
         close_tx = None
         close_type = None
 
-    print(json.dumps({
-        "funding_tx": funding_tx,
-        "close_tx": close_tx,
-        "status": 'open' if close_tx is None else 'closed',
-        'lifetime': lifetime,
-        "close_type": close_type,
-        'penalty': penalty,
-    }, indent=2))
+    print(
+        json.dumps(
+            {
+                "funding_tx": funding_tx,
+                "close_tx": close_tx,
+                "status": "open" if close_tx is None else "closed",
+                "lifetime": lifetime,
+                "close_type": close_type,
+                "penalty": penalty,
+            },
+            indent=2,
+        )
+    )
+
+
 
 
 if __name__ == "__main__":
